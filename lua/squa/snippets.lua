b03fa43861cc8a7818665df19c16f3ed68f3f554
Timothy <timo-drexler@online.de>
2025-09-16 16:59:03 +0200

    changed and added some Snippets for FileType == scala

--- lua/squa/snippets.lua
+++ lua/squa/snippets.lua
@@ -10,15 +10,15 @@ local d = ls.dynamic_node
 local r = ls.restore_node
 
 vim.keymap.set({ 'i', 's' }, '<C-j>', function()
-	if ls.choice_active() then
-		ls.change_choice(1)
-	end
+    if ls.choice_active() then
+        ls.change_choice(1)
+    end
 end, { silent = true, desc = "luasnip change_choice(1)" })
 
 vim.keymap.set({ 'i', 's' }, '<C-k>', function()
-	if ls.choice_active() then
-		ls.change_choice(-1)
-	end
+    if ls.choice_active() then
+        ls.change_choice(-1)
+    end
 end, { silent = true, desc = "luasnip change_choice(-1)" })
 
 ls.add_snippets("scala", {
@@ -30,6 +30,54 @@ ls.add_snippets("scala", {
         t("foreach("),
         i(1, "action"),
         t(")")
+    }
+    ),
+    snip({
+        trig = "min",
+        name = "min",
+        desc = "find min"
+    }, {
+        t("min")
+    }
+    ),
+    snip({
+        trig = "max",
+        name = "max",
+        desc = "find max"
+    }, {
+        t("max")
+    }
+    ),
+    snip({
+        trig = "toList",
+        name = "toList",
+        desc = "toList"
+    }, {
+        t("toList")
+    }
+    ),
+    snip({
+        trig = "toSeq",
+        name = "toSeq",
+        desc = "toSeq"
+    }, {
+        t("toSeq")
+    }
+    ),
+    snip({
+        trig = "flatten",
+        name = "flatten",
+        desc = "flatten"
+    }, {
+        t("flatten")
+    }
+    ),
+    snip({
+        trig = "distinct",
+        name = "distinct",
+        desc = "distinct removes duplicates"
+    }, {
+        t("distinct")
     }
     ),
     snip({
@@ -53,6 +101,14 @@ ls.add_snippets("scala", {
     }
     ),
     snip({
+        trig = "identity",
+        name = "identity",
+        desc = "identity: i => i"
+    }, {
+        t("identity")
+    }
+    ),
+    snip({
         trig = "filter",
         name = "filter",
         desc = "Create a filter switching between '==', t'!=', '>', '<', '>=' and '<=' with <C-j> and <C-k>"
@@ -63,6 +119,16 @@ ls.add_snippets("scala", {
         c(2, { t("=="), t("!="), t(">"), t("<"), t(">="), t("<=") }),
         t(" "),
         i(3, "value"),
+        t(")")
+    }
+    ),
+    snip({
+        trig = "mapValues",
+        name = "mapValues",
+        desc = "mapValues takes a function B => C, where C is the new type for the values."
+    }, {
+        t("mapValues(("),
+        i(1, "C"),
         t(")")
     }
     ),
@@ -153,107 +219,37 @@ ls.add_snippets("scala", {
     }
     ),
     snip({
-        trig = "list",
-        name = "list1",
-        desc = "Create a list"
+        trig = "Buffer",
+        name = "Buffer",
+        desc = "Create a Buffer"
     }, {
-        t("val "),
-        i(1, "listname"),
-        t(": List["),
-        i(2, "type"),
-        t("] = List("),
-        i(3, "elements"),
-        t(")")
-    }
-    ),
-    snip({
-        trig = "list",
-        name = "list2",
-        desc = "Create a list"
-    }, {
-        t("var "),
-        i(1, "listname"),
-        t(": List["),
-        i(2, "type"),
-        t("] = List("),
-        i(3, "elements"),
-        t(")")
-    }
-    ),
-    snip({
-        trig = "List",
-        name = "List",
-        desc = "Create a List"
-    }, {
-        t("List("),
-        i(1, "elements"),
-        t(")")
-    }
-    ),
-    snip({
-        trig = "seq",
-        name = "seq1",
-        desc = "Create a seq"
-    }, {
-        t("val "),
-        i(1, "seqname"),
-        t(": Seq["),
-        i(2, "type"),
-        t("] = Seq("),
-        i(3, "elements"),
-        t(")")
-    }
-    ),
-    snip({
-        trig = "seq",
-        name = "seq2",
-        desc = "Create a seq"
-    }, {
-        t("var "),
-        i(1, "seqname"),
-        t(": Seq["),
-        i(2, "type"),
-        t("] = Seq("),
-        i(3, "elements"),
-        t(")")
-    }
-    ),
-    snip({
-        trig = "Seq",
-        name = "Seq",
-        desc = "Create a Seq"
-    }, {
-        t("Seq("),
-        i(1, "elements"),
-        t(")")
-    }
-    ),
-    snip({
-        trig = "arraybuffer",
-        name = "arraybuffer1",
-        desc = "Create a arraybuffer"
-    }, {
-        t("val "),
-        i(1, "arraybuffername"),
-        t(": ArrayBuffer["),
-        i(2, "type"),
-        t("] = ArrayBuffer("),
-        i(3, "elements"),
-        t(")")
-    }
-    ),
-    snip({
-        trig = "arraybuffer",
-        name = "arraybuffer2",
-        desc = "Create a arraybuffer"
-    }, {
-        t("var "),
-        i(1, "arraybuffername"),
-        t(": ArrayBuffer["),
-        i(2, "type"),
-        t("] = ArrayBuffer("),
-        i(3, "elements"),
-        t(")")
+        c(1, { {
+            t("val "),
+            i(1, "Buffername"),
+            t(": Buffer["),
+            i(2, "type"),
+            t("] = Buffer("),
+            i(3, "elements"),
+            t(")") }
+        , {
+            t("var "),
+            i(1, "Buffername"),
+            t(": Buffer["),
+            i(2, "type"),
+            t("] = Buffer("),
+            i(3, "elements"),
+            t(")") }
+        , {
+            t("Buffer("),
+            i(1, "elements"),
+            t(")") }
+        , {
+            t("Buffer["),
+            i(2, "type"),
+            t("]("),
+            i(3, "elements"),
+            t(")") }
+        }),
     }
     ),
     snip({
@@ -261,37 +257,67 @@ ls.add_snippets("scala", {
         name = "ArrayBuffer",
         desc = "Create a ArrayBuffer"
     }, {
-        t("ArrayBuffer("),
-        i(1, "elements"),
-        t(")")
+        c(1, { {
+            t("val "),
+            i(1, "ArrayBuffername"),
+            t(": ArrayBuffer["),
+            i(2, "type"),
+            t("] = ArrayBuffer("),
+            i(3, "elements"),
+            t(")") }
+        , {
+            t("var "),
+            i(1, "ArrayBuffername"),
+            t(": ArrayBuffer["),
+            i(2, "type"),
+            t("] = ArrayBuffer("),
+            i(3, "elements"),
+            t(")") }
+        , {
+            t("ArrayBuffer("),
+            i(1, "elements"),
+            t(")") }
+        , {
+            t("ArrayBuffer["),
+            i(2, "type"),
+            t("]("),
+            i(3, "elements"),
+            t(")") }
+        }),
     }
     ),
     snip({
-        trig = "array",
-        name = "array1",
-        desc = "Create a array"
+        trig = "List",
+        name = "List",
+        desc = "Create a List"
     }, {
-        t("val "),
-        i(1, "arrayname"),
-        t(": Array["),
-        i(2, "type"),
-        t("] = Array("),
-        i(3, "elements"),
-        t(")")
-    }
-    ),
-    snip({
-        trig = "array",
-        name = "array2",
-        desc = "Create a array"
-    }, {
-        t("var "),
-        i(1, "arrayname"),
-        t(": Array["),
-        i(2, "type"),
-        t("] = Array("),
-        i(3, "elements"),
-        t(")")
+        c(1, { {
+            t("val "),
+            i(1, "Listname"),
+            t(": List["),
+            i(2, "type"),
+            t("] = List("),
+            i(3, "elements"),
+            t(")") }
+        , {
+            t("var "),
+            i(1, "Listname"),
+            t(": List["),
+            i(2, "type"),
+            t("] = List("),
+            i(3, "elements"),
+            t(")") }
+        , {
+            t("List("),
+            i(1, "elements"),
+            t(")") }
+        , {
+            t("List["),
+            i(2, "type"),
+            t("]("),
+            i(3, "elements"),
+            t(")") }
+        }),
     }
     ),
     snip({
@@ -299,47 +325,67 @@ ls.add_snippets("scala", {
         name = "Array",
         desc = "Create a Array"
     }, {
-        t("Array("),
-        i(1, "elements"),
-        t(")")
+        c(1, { {
+            t("val "),
+            i(1, "Arrayname"),
+            t(": Array["),
+            i(2, "type"),
+            t("] = Array("),
+            i(3, "elements"),
+            t(")") }
+        , {
+            t("var "),
+            i(1, "Arrayname"),
+            t(": Array["),
+            i(2, "type"),
+            t("] = Array("),
+            i(3, "elements"),
+            t(")") }
+        , {
+            t("Array("),
+            i(1, "elements"),
+            t(")") }
+        , {
+            t("Array["),
+            i(2, "type"),
+            t("]("),
+            i(3, "elements"),
+            t(")") }
+        }),
     }
     ),
     snip({
-        trig = "buffer",
-        name = "buffer1",
-        desc = "Create a buffer"
+        trig = "Seq",
+        name = "Seq",
+        desc = "Create a Seq"
     }, {
-        t("val "),
-        i(1, "buffername"),
-        t(": Buffer["),
-        i(2, "type"),
-        t("] = Buffer("),
-        i(3, "elements"),
-        t(")")
-    }
-    ),
-    snip({
-        trig = "buffer",
-        name = "buffer2",
-        desc = "Create a buffer"
-    }, {
-        t("var "),
-        i(1, "buffername"),
-        t(": Buffer["),
-        i(2, "type"),
-        t("] = Buffer("),
-        i(3, "elements"),
-        t(")")
-    }
-    ),
-    snip({
-        trig = "Buffer",
-        name = "Buffer",
-        desc = "Create a Buffer"
-    }, {
-        t("Buffer("),
-        i(1, "elements"),
-        t(")")
+        c(1, { {
+            t("val "),
+            i(1, "Seqname"),
+            t(": Seq["),
+            i(2, "type"),
+            t("] = Seq("),
+            i(3, "elements"),
+            t(")") }
+        , {
+            t("var "),
+            i(1, "Seqname"),
+            t(": Seq["),
+            i(2, "type"),
+            t("] = Seq("),
+            i(3, "elements"),
+            t(")") }
+        , {
+            t("Seq("),
+            i(1, "elements"),
+            t(")") }
+        , {
+            t("Seq["),
+            i(2, "type"),
+            t("]("),
+            i(3, "elements"),
+            t(")") }
+        }),
     }
     ),
 })
